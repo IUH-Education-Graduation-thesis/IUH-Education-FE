ea3ce8f43a778a965c2bc86c694d0139fe5f5bfc
Hoan Truong <hoantruong6814@gamil.com>
2022-03-09 23:07:58 +0700

    handle build layout filter for khoa vien page

---
+++ src/components/Khoa/FilterExpand/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Button, Form, Input } from "antd";
+import {
+  SearchOutlined,
+  ClearOutlined,
+} from "@ant-design/icons";
+
+const prefix = "sinh-vien--filter";
+
+
+
+const { useForm } = Form;
+
+const ExpandFilter = ({
+  onAddAStudentClick,
+  onAddWithFileClick,
+  onFilterChange,
+  currentFilterData,
+  onClear,
+}) => {
+  const [form] = useForm();
+
+  /**
+   * function
+   * ==========================================
+   */
+
+  const handleFilterCHange = (payload) => {
+    const _fieldChange = {
+      [payload?.[0]?.name?.[0]]: payload?.[0]?.value,
+    };
+
+    const _currentFilterData = {
+      ...currentFilterData,
+      ..._fieldChange,
+    };
+
+    onFilterChange(_fieldChange, _currentFilterData, payload?.name?.[0]);
+  };
+
+  const handleClearFilter = () => {
+    form.resetFields();
+    onClear();
+  };
+
+  /**
+   * render view
+   * ==================================
+   */
+
+  return (
+    <Form form={form} onFieldsChange={handleFilterCHange} className={prefix}>
+      <div className={`${prefix}__top`}>
+        <div className={`${prefix}__top__left`}>
+          <Form.Item name="id">
+            <Input
+              prefix={<SearchOutlined />}
+              placeholder="Nhập id khoa viện..."
+            />
+          </Form.Item>
+          <Form.Item name="ten">
+            <Input
+              prefix={<SearchOutlined />}
+              placeholder="Nhập tên khoa viện..."
+            />
+          </Form.Item>
+        </div>
+
+        <div className={`${prefix}__top__right`}>
+          <Button onClick={onAddAStudentClick} type="primary">
+            {" "}
+            + Thêm 1 khoa viện
+          </Button>
+          <Button onClick={onAddWithFileClick} type="ghost">
+            {" "}
+            + Thêm bằng file excel
+          </Button>
+          <ClearOutlined onClick={handleClearFilter} />
+        </div>
+      </div>
+    </Form>
+  );
+};
+
+export default ExpandFilter;
+
+ExpandFilter.propTypes = {
+  onAddAStudentClick: PropTypes.func,
+  onAddWithFileClick: PropTypes.func,
+  onFilterChange: PropTypes.func,
+  currentFilterData: PropTypes.objectOf(PropTypes.any).isRequired,
+  onClear: PropTypes.func,
+};
+
+ExpandFilter.defaultProps = {
+  onAddAStudentClick: () => {},
+  onAddWithFileClick: () => {},
+  onFilterChange: () => {},
+  currentFilterData: {},
+  onClear: () => {},
+};
--- src/components/Khoa/index.js
+++ src/components/Khoa/index.js
@@ -1,62 +1,72 @@
 import React, { useState } from "react";
-import { Button, Table, Modal } from 'antd';
-import './Khoa.scss'
-import ModalAddKhoa from './FormAddKhoa'
+import { Button, Table, Modal } from "antd";
+import "./Khoa.scss";
+import ModalAddKhoa from "./FormAddKhoa";
+import FilterExpand from "./FilterExpand";
+
 const KhoaComponent = () => {
   const [visibleModal1, setVisibleModal1] = useState(false);
   const [visibleModal, setVisibleModal] = useState(false);
   const [khoa, setKhoa] = useState({});
   const columns = [
     {
-      title: 'Mã khoa',
-      dataIndex: 'maKhoa',
-      key: 'maKhoa',
+      title: "Mã khoa",
+      dataIndex: "maKhoa",
+      key: "maKhoa",
       width: 100,
     },
     {
-      title: 'Tên khoa',
-      dataIndex: 'tenKhoa',
-      key: 'tenKhoa',
+      title: "Tên khoa",
+      dataIndex: "tenKhoa",
+      key: "tenKhoa",
       width: 400,
     },
     {
-      title: 'Mô tả',
-      dataIndex: 'moTa',
-      key: 'moTa',
+      title: "Mô tả",
+      dataIndex: "moTa",
+      key: "moTa",
       width: 300,
     },
     {
-      title: 'Thao tác',
-      key: 'thaoTac',
+      title: "Thao tác",
+      key: "thaoTac",
       width: 300,
       render: (e) => (
         <div>
           <Button danger onClick={() => handlerEditButton(e)}>
             Chỉnh sửa
           </Button>
-          <Button style={{marginLeft:10}}>Xóa</Button>
+          <Button style={{ marginLeft: 10 }}>Xóa</Button>
         </div>
       ),
     },
   ];
+
   const handlerEditButton = (khoa) => {
     setKhoa(khoa);
     setVisibleModal1(true);
   };
+
   const data = [];
   for (let i = 0; i < 30; i++) {
     data.push({
       key: i,
       maKhoa: `${i}`,
       tenKhoa: `Kinh doanh quốc tế`,
-      moTa: 'New York No. 1 Lake Park',
+      moTa: "New York No. 1 Lake Park",
     });
   }
+
   return (
-    <div className='khoa'>
-      <h1>DANH SÁCH KHOA</h1>
-      <Button className='ant-btn-primary' type="primary"  onClick={() => setVisibleModal(true)}>+ Thêm khoa</Button>
-      <Table className='ant-table-wrapper' columns={columns} dataSource={data} />
+    <div className="khoa">
+      <h3>DANH SÁCH KHOA</h3>
+      <FilterExpand />
+
+      <Table
+        className="ant-table-wrapper"
+        columns={columns}
+        dataSource={data}
+      />
       <ModalAddKhoa
         type="add"
         visible={visibleModal}
@@ -66,9 +76,7 @@ const KhoaComponent = () => {
         type="sua"
         visible={visibleModal1}
         closeModal={setVisibleModal1}
-        data={
-          khoa
-        }
+        data={khoa}
       />
     </div>
   );
